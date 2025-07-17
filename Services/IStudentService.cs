cecfd2740974ae889c8ceac4448a3b0520e32c4d
Abdelrahman <abdoshady550@gmail.com>
2025-07-18 00:54:07 +0300

    Add IStudentService interface and StudentService implementation
    Introduces the `IStudentService` interface for managing `Student` entities with methods for retrieving, adding, updating, and deleting students. Implements the `StudentService` class that interacts with the database using `AppDbContext`. Updates include necessary using directives for proper namespace access.

---
+++ Services/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿using AcademyProject_API.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Movie_Api.Services
+{
+    public interface IStudentService
+    {
+        public Task<IEnumerable<Student>> GetAll();
+        public Task<Student> GetById(int id);
+        public Task<Student> Add(Student Student);
+        public Student Update(Student Student);
+        public Student Delete(Student Student);
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,55 @@
+﻿namespace Movie_Api.Services
+{
+    using AcademyProject_API.Model.Data;
+    using AcademyProject_API.Model.Dtos;
+    using AcademyProject_API.Model.Entities;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StudentService : IStudentService
+    {
+        private readonly AppDbContext _context;
+
+        public StudentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Student>> GetAll()
+        {
+            return await _context.Students.OrderBy(e => e.FName).ToListAsync();
+        }
+
+        public async Task<Student> GetById(int id)
+        {
+            var item = await _context.Students.FindAsync(id);
+            if (item == null)
+                return null;
+
+            return item;
+        }
+
+        public async Task<Student> Add(Student Student)
+        {
+
+            await _context.Students.AddAsync(Student);
+            await _context.SaveChangesAsync();
+            return Student;
+        }
+
+        public Student Delete(Student Student)
+        {
+
+            _context.Students.Remove(Student);
+            _context.SaveChanges();
+            return Student;
+        }
+
+        public Student Update(Student Student)
+        {
+            _context.Update(Student);
+            _context.SaveChanges();
+            return Student;
+        }
+    }
+}
