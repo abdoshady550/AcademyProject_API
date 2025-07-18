d6dfccb7e25a7fd3290da074bc50eddfa579238b
Abdelrahman <abdoshady550@gmail.com>
2025-07-18 12:57:52 +0300

    Add InstractorController for managing Instractor entities
    Introduces the `InstractorController` class in the
    `AcademyProject_API.Controllers` namespace. This controller
    inherits from `BaseController<Instractor>` and includes a
    constructor that accepts an `IGenaricRepo<Instractor>` service.
    Several using directives for necessary namespaces are also added.

---
+++ Controllers/InstractorController.cs
@@ -0,0 +1,16 @@
+﻿using AcademyProject_API.Model.Entities;
+using AcademyProject_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyProject_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstractorController : BaseController<Instractor>
+    {
+        public InstractorController(IGenaricRepo<Instractor> service) : base(service)
+        {
+        }
+    }
+}
