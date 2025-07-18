c1052a32ad4cd19ae3932f9498c7f07991b7ad51
Abdelrahman <abdoshady550@gmail.com>
2025-07-18 12:56:43 +0300

    Update repository structure and add generic services
    - Updated `.gitignore` to include `AcademyProject_API.http`.
    - Removed host address and request details from `AcademyProject_API.http`.
    - Implemented `GenaricService<T>` class in `GenaricService.cs` for common database operations.
    - Defined `IGenaricRepo<T>` interface in `IGenaricRepo.cs` to standardize repository methods.

---
+++ Services/GenaricService.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Threading.Tasks;
+using AcademyProject_API.Model.Data;
+using AcademyProject_API.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademyProject_API.Services
+{
+    public class GenaricService<T> : IGenaricRepo<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+
+        public GenaricService(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            var item = await _dbSet.FindAsync(id);
+            if (item == null)
+                return null;
+
+            return item;
+        }
+
+        public async Task<T> Add(T item)
+        {
+
+            await _dbSet.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<T> Delete(T item)
+        {
+
+            _dbSet.Remove(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<T> UpdateAsync(T item, T itemUpdated)
+        {
+
+            _context.Entry(item).CurrentValues.SetValues(itemUpdated);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
---
+++ Services/IGenaricRepo.cs
@@ -0,0 +1,13 @@
+﻿using AcademyProject_API.Model.Entities;
+
+namespace AcademyProject_API.Services
+{
+    public interface IGenaricRepo<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T> GetById(int id);
+        public Task<T> Add(T item);
+        public Task<T> UpdateAsync(T item, T itemUpdated);
+        public Task<T> Delete(T tiem);
+    }
+}
